58e2eec6ee19079e2bcd757ea0aec862ec903ed3
Dali <oscardali370@gmail.com>
2022-09-11 21:15:28 -0500

    Width and height added

--- src/app/image-view-component/image-view-component.component.ts
+++ src/app/image-view-component/image-view-component.component.ts
@@ -9,6 +9,8 @@ export class ImageViewComponentComponent implements OnInit {
 
   @Input() url: string = "https://i.gifer.com/origin/d0/d0fc7bdc9a240be0950efb47f3c90f81.gif";
   @Input() id: string = "empty";
+  @Input() width: string = "250";
+  @Input() height: string = "250";
 
   ngOnInit(): void {
   }
